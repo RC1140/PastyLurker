73e274b9d1b69d6506e7b6db838032390d7d9cb7
RC1140 <jameel@republiccommandos.co.za>
2011-07-02 00:00:21 +0200

    Reset code

--- watchLoader.js
+++ watchLoader.js
@@ -1,37 +1,35 @@
-//var argv = require('optimist')
-            //.usage('Usage: --watchString=[consumer watchString] --username=[consumer username]')
-            //.demand(['watchString']).argv;
+var argv = require('optimist')
+            .usage('Usage: --watchString=[consumer watchString] --username=[consumer username]')
+            .demand(['watchString']).argv;
 var dbMan = require('./dbManager');
 
-dbMan.scrapeWatchModel.find({},function(err,docs){
-     docs.forEach(function(doc){
-        doc.active = true;    
-        doc.save(function(err){
-           console.log('saved'); 
-            });
-     });        
-});
-//var watchModel = new dbMan.scrapeWatchModel();
-//watchModel.watchString = argv.watchString;
-//watchModel.active = true;
-//dbMan.webUsersModel.findOne({'username':argv.username},function(err,user){
-    //if(err){
-        //console.log(err);
-        //process.exit();
-    //};
-    //if(user){
-        //watchModel.user.push(user);
-        //watchModel.save(function(err){
-            //if(err){
-                //console.log(err);
-            //}
-            //console.log('watch saved');
-            //process.exit();
-        //});
-    //}else{
-        //console.log('no user found');
-        //process.exit();
-    //};
-           
+//dbMan.scrapeWatchModel.find({},function(err,docs){
+     //docs.forEach(function(doc){
+        //doc.active = true;    
+        //doc.save(function(err){
+           //console.log('saved'); 
+            //});
+     //});        
 //});
-
+var watchModel = new dbMan.scrapeWatchModel();
+watchModel.watchString = argv.watchString;
+watchModel.active = true;
+dbMan.webUsersModel.findOne({'username':argv.username},function(err,user){
+    if(err){
+        console.log(err);
+        process.exit();
+    };
+    if(user){
+        watchModel.user.push(user);
+        watchModel.save(function(err){
+            if(err){
+                console.log(err);
+            }
+            console.log('watch saved');
+            process.exit();
+        });
+    }else{
+        console.log('no user found');
+        process.exit();
+    };
+});
